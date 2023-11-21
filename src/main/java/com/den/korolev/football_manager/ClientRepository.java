565be4022bd2361322e1e9a47fe7d01a5b93db4c
Denis Korolev <den.korolev.1999@bk.ru>
2023-11-21 13:36:56 +0300

    Initial commit

---
+++ src/main/java/com/den/korolev/football_manager/ClientRepository.java
@@ -0,0 +1,6 @@
+package com.den.korolev.football_manager;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
